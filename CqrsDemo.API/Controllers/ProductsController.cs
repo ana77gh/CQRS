bc9553aa9fa792d4a178fe6a2306864553e7f0f7
Madrin Arfilida <anadliarfilida77@gmail.com>
2026-05-13 08:33:32 +0700

    Add ProductsController with MediatR-based CQRS endpoints
    Introduced ProductsController to handle product-related API requests. The controller uses MediatR to implement CQRS, providing POST and GET endpoints for creating and retrieving products, respectively. Dependency injection is used for IMediator, and standard ASP.NET Core attributes are applied for routing and API behavior.

---
+++ CqrsDemo.API/Controllers/ProductsController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MediatR;
+using CqrsDemo.Application.Features.Products.Commands.CreateProduct;
+using CqrsDemo.Application.Features.Products.Queries.GetAllProducts;
+
+namespace CqrsDemo.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public ProductsController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(CreateProductCommand command)
+        {
+            var productId = await _mediator.Send(command);
+            return Ok(productId);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var result = await _mediator.Send(new GetAllProductsQuery());
+            return Ok(result);
+        }
+    }
+}
